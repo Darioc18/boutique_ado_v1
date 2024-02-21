1444133ae577598121f6669aab1524d4db883cde
Dario <dariocovella@gmail.com>
2024-02-21 00:26:38 +0000

    deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ci-pp5-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ci-pp5-boutique-ado-6393678e3bac.herokuapp.com', 'localhost']
 
 
 # Application definition
