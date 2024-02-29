103e87ea71bcfb62729499b3efc9c8269d847459
Dario <dariocovella@gmail.com>
2024-02-29 23:26:19 +0000

    062 base.css and login/reg refactor

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['ci-pp5-boutique-ado-6393678e3bac.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ci-pp5-boutique-ado-6393678e3bac.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
